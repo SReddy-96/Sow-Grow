d0f52dae1c5306d7166d5c05476da816cbbf7952
Steven Reddy <steveng.reddy@gmail.com>
2024-05-30 11:06:56 +0100

    update settings PT4

--- SowAndGrow/settings.py
+++ SowAndGrow/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['https://sow-and-grow.onrender.com']
+ALLOWED_HOSTS = ['sow-and-grow.onrender.com']
 
 
 # Application definition
